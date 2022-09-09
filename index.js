2331f518f25fd6a2963fc8213077ac2bfb36a56e
manoahLinks <manoahluka@gmail.com>
2022-09-09 04:28:31 +0100

    adding static (views/public)

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 let express = require('express'),
     app     = express()
 
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + "/views"))
+app.use(express.static(__dirname + "/public"))
 
-
+app.get('/', (req, res)=>{
+    res.render('index')
+})
 
 app.listen(5000, ()=>{
     console.log('server is connected for sales-app')
